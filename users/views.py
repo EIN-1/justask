1bfbbd27c987dafb8e851d3661050b2f1cb3be0b
Elsie Nagawa <198elsie@gmail.com>
2024-07-04 18:48:43 +0000

    Added Create Question form to home

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from forms import UserRegistrationForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
