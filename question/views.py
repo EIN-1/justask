b8fd18114506b4ef33956b0d023d7a492903cdb4
Elsie Nagawa <198elsie@gmail.com>
2024-07-04 14:59:12 +0000

    Added questions, views, templates and urls plus added question to the root url path

--- question/views.py
+++ question/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question
 
 # Create your views here.
+def home(request):
+    questions = Question.objects.all().order_by('-created_at')
+    return render(request, 'questions/home.html',{'questions': questions})
+
+def question_detail(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    return render(request, 'questions/details.html', {'question':question})
