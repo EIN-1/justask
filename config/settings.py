5fc4689f6c8b1203f5cb1eace1dce451dd9d35f7
Elsie Nagawa <198elsie@gmail.com>
2024-07-04 13:58:55 +0000

    Added Institude url to allow viewing in web browser

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b7qf#un**w^rjra0ej3ulyk61yy_e92vzze@m9ma7muodxnaf-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["8000-ein1-justask-2lb39gbyi8i.ws.codeinstitute-ide.net"]
 
 
 # Application definition
