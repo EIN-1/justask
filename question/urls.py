0ea628a46b8b23ea13f190cf487433251533b877
Elsie Nagawa <198elsie@gmail.com>
2024-07-05 07:24:56 +0000

    Added the add comment logic with JQuery

--- question/urls.py
+++ question/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('<int:pk>/', views.question_detail, name='question_detail'),
-    path('get-comments/', views.get_comments, name='get_comments')
+    path('get-comments/', views.get_comments, name='get_comments'),
+    path('add-comment/', views.add_comment, name='add_comment')
 ]
--- question/views.py
+++ question/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.db.models import Count
-from forms import QuestionForm
+from forms import QuestionForm, CommentForm
 from comments.models import Comment
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def home(request):
@@ -25,4 +26,19 @@ def question_detail(request, pk):
 def get_comments(request):
     question_id = request.GET.get('question_id')
     comments = Comment.objects.filter(question_id=question_id).order_by('created_at')
-    return render(request, 'partials/comments.html', {'comments': comments})
+    return render(request, 'partials/comments.html', {'comments': comments, 'question_id': question_id})
+
+@require_POST
+def add_comment(request):
+    question_id = request.POST.get('question_id')
+    print(question_id)
+    question = Question.objects.get(id=question_id)
+    form = CommentForm(request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.question = question
+        comment.author = request.user
+        comment.save()
+        return render(request, 'partials/comment.html', {'comment': comment})
+    return JsonResponse({'error': form.errors}, status=400)
