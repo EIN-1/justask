bbb5c4659eb016181a3a4ed2f789aa50838f9205
Elsie Nagawa <198elsie@gmail.com>
2024-07-05 04:28:14 +0000

    Added User login and logout

--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,7 @@ STATIC_ROOT = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from forms import UserRegistrationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 def register(request):
@@ -13,4 +16,12 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+    authentication_form = AuthenticationForm
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('home')
 
