260383f78e5cefffef906f08abe6b9df7aef236f
Elsie Nagawa <198elsie@gmail.com>
2024-07-08 19:41:24 +0000

    Migrated the postgres database using python manage.py migrate to create the tables. Added comments to the populate_questions.py and run the python manage.py populate_questions command to add faked data and faked timezone from 1 year ago to now.

--- question/management/commands/populate_questions.py
+++ question/management/commands/populate_questions.py
@@ -1,7 +1,7 @@
 import random
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from question.models import Category, Question,Reaction
+from question.models import Category, Question, Reaction
 from comments.models import Comment
 from faker import Faker
 from datetime import timedelta
@@ -12,9 +12,18 @@ class Command(BaseCommand):
     help = 'Populate the database with fake data'
 
     def handle(self, *args, **kwargs):
+        """
+            This is the function that is called when the populate_questions command is run
+        """
         fake = Faker()
 
         # Create categories
+        # 1. create an empty list of categories
+        # 2. Inform the user that categories are being created
+        # 3. Loop 5 times
+        #    3.a Create a category with a random word using faker
+        #    3.b Append the created category to the categories array
+
         categories = []
         self.stdout.write("Generating categories....")
         for _ in range(5):
@@ -22,6 +31,11 @@ class Command(BaseCommand):
             categories.append(category)
 
         # Create users
+        # 1. create an empty list of users
+        # 2. Inform the user that users are being created
+        # 3. Loop 10 times
+        #    3.a Create a user with a random username and email using faker and a password
+        #    3.b Append the created user to the user array
         users = []
         self.stdout.write("Generating users....")
         for _ in range(10):
@@ -33,6 +47,12 @@ class Command(BaseCommand):
             users.append(user)
 
         # Create questions
+        # 1. create an empty list of questions
+        # 2. Inform the user that questions are being created
+        # 3. Loop 20 times
+        #    3.a Create a random time (created_at) between 1 year ago and today
+        #    3.b Create a question with a random title, content, author, category and the created_at time, and a rondom author
+        #    3.c Add the question to the questions list
         questions = []
         self.stdout.write("Generating questions....")
         for _ in range(20):
@@ -48,6 +68,11 @@ class Command(BaseCommand):
             questions.append(question)
 
         # Create reactions
+        # 1. Inform the admin that reactions are being created
+        # 2. Loop through all questions in the questions list
+        #    2.a Loop a random number of times in the question
+        #       2.a.1 Try to create a random reaction either like or dislike 
+        #             by a random user or skip
         self.stdout.write("Generating reactions....")
         for question in questions:
             for _ in range(random.randint(0, 10)):
@@ -61,6 +86,10 @@ class Command(BaseCommand):
                     continue
 
         # Create comments
+        # 1. Inform the admin that comments are being created
+        # 2. Loop through all questions in the questions list
+        #    2.a Loop a random number of times in the question
+        #       2.a.1 Create a comment by a random user and random content
         self.stdout.write("Generating comments....")
         for question in questions:
             for _ in range(random.randint(0, 5)):
@@ -70,4 +99,5 @@ class Command(BaseCommand):
                     content=fake.paragraph()
                 )
 
+        # Imform admin that the database has been populated with data successfully
         self.stdout.write(self.style.SUCCESS('Successfully populated the database with fake data'))
